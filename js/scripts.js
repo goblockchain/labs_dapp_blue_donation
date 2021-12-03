9bead9fa8ad29c931b6add77346879f39ce51916
wallace azevedo <wallaceazevedo17@gmail.com>
2021-12-03 17:05:43 -0300

    new page and up dates
    nova pagina para cadrastar uma nft. up date botoes home

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,23 @@ function validarFormularioONG(){
             alert("Preencha o seu Núemero de Telefone");
         }
         return;
-}
+};
+
+
+const Web3 = require("web3");
+function conectarMetamask(){
+    const ethEnabled = async () => {
+    if (window.ethereum) {
+        await window.ethereum.send('eth_requestAccounts');
+        window.web3 = new Web3(window.ethereum);
+        return true;
+    }
+    return false;
+    }
+};
+
+
+
+
+
+
