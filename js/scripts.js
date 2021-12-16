8a6c258e5795e843c3a2ec3d5f7df03e73e6cebd
unknown <wallaceazevedo17@gmail.com>
2021-12-15 21:56:50 -0300

    new connection with metamask

--- js/scripts.js
+++ js/scripts.js
@@ -18,21 +18,3 @@ function validarFormularioONG(){
         return;
 };
 
-
-const Web3 = require("web3");
-function conectarMetamask(){
-    const ethEnabled = async () => {
-    if (window.ethereum) {
-        await window.ethereum.send('eth_requestAccounts');
-        window.web3 = new Web3(window.ethereum);
-        return true;
-    }
-    return false;
-    }
-};
-
-
-
-
-
-
